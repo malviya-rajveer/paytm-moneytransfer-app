dd725bcddc2af93d33fa7342dbf0bc45f4cac41b
Rajveer malviya <malviyarajveer777@gmail.com>
2024-08-07 15:51:54 +0530

    p2p transaction added

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({
                 <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
-                <SidebarItem href={"/peer2peer"} icon={<PeerTwoPeer />} title="hello" />
+                <SidebarItem href={"/p2pTransfer"} icon={<PeerTwoPeer />} title="p2pTranfer" />
             </div>
         </div>
             {children}
--- apps/user-app/app/(dashboard)/peer2peer/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function(){
-    return <div>
-        hii therre jai shree shyam
-    </div>
-}
